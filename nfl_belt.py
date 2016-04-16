ccddbeeca1bbace2b5cd92ef794e60bfb2804777
Erik Kieckhafer <ekieckhafer@mediatemple.net>
2016-04-15 18:13:03 -0700

    Changed STL Rams to the LA Rams

--- nfl_belt.py
+++ nfl_belt.py
@@ -60,7 +60,7 @@ teams = [
  "New Orleans Saints",
  "Tampa Bay Buccaneers",
 
- "St. Louis Rams",
+ "Los Angeles Rams",
  "San Francisco 49ers",
  "Arizona Cardinals",
  "Seattle Seahawks"]
@@ -110,7 +110,7 @@ edge_sets = {t: [w for w in weeks if w] for t, weeks in edge_sets.items()}
 
 height = 1000
 width = 1200
-stroke_width = 0.007 * width
+stroke_width = 0.0007 * width
 gutter= [50,190,60,30]
 div_sep = 0.02 * width
 conf_sep = 0.01 * width
